9adf1a3062c6d5c73867afda202690d53bf61027
pahliana <phliana007@gmail.com>
2021-04-07 13:09:01 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Pahliana";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+}
